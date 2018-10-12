1a09a6844e47142846f69db93f93e5106a44de06
William Savoie <WillSavvy@williams-macbook-pro.local>
2018-10-12 14:09:42 -0400

    Initial Commit

---
+++ King Philip App/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  King Philip App
+//
+//  Created by William Savoie on 10/12/18.
+//  Copyright © 2018 King Philip. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
